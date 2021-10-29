1cb6af5702d7e45ae287e23286bcf6c8565a3216
ByronAP <7310089+ByronAP@users.noreply.github.com>
2021-10-28 22:47:24 -0400

    add appdbcontext

--- src/Alirta/Services/ChainUpdaterService.cs
+++ src/Alirta/Services/ChainUpdaterService.cs
@@ -1,4 +1,5 @@
 ﻿using Alirta.Contracts;
+using Alirta.DbContexts;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.Threading;
@@ -10,11 +11,13 @@ namespace Alirta.Services
     {
         private readonly IChainConfig _chainConfig;
         private readonly ILogger<ChainUpdaterService> _logger;
+        private readonly AppDbContext _appDbContext;
 
-        public ChainUpdaterService(IChainConfig chainConfig, ILogger<ChainUpdaterService> logger)
+        public ChainUpdaterService(IChainConfig chainConfig, ILogger<ChainUpdaterService> logger, AppDbContext appDbContext)
         {
             _chainConfig = chainConfig;
             _logger = logger;
+            _appDbContext = appDbContext;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
