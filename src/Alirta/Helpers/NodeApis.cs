4fe71a1177aa20c193f6ec926f84e41a5bef7602
ByronAP <7310089+ByronAP@users.noreply.github.com>
2021-10-29 09:56:49 -0400

    add api helpers class

---
+++ src/Alirta/Helpers/NodeApis.cs
@@ -0,0 +1,50 @@
+﻿using Alirta.Contracts;
+using Alirta.Models;
+using ChiaApi;
+using System.IO;
+
+namespace Alirta.Helpers
+{
+    internal static class NodeApis
+    {
+        internal static FullNodeApiClient GetFullNodeApiClient(IChainConfig chainConfig)
+        {
+            var certsPath = FileSystem.GetChainCertDirectoryPath(chainConfig.ChainFolder, chainConfig.Network, NodeEntryPoint.Full_Node);
+            var certFilePath = Path.Combine(certsPath, "private_full_node.crt");
+            var keyFilePath = Path.Combine(certsPath, "private_full_node.key");
+
+            var nodeApiConfig = new ChiaApiConfig(certFilePath, keyFilePath, "localhost", chainConfig.FullNodePort);
+            return new FullNodeApiClient(nodeApiConfig);
+        }
+
+        internal static FarmerApiClient GetFarmerApiClient(IChainConfig chainConfig)
+        {
+            var certsPath = FileSystem.GetChainCertDirectoryPath(chainConfig.ChainFolder, chainConfig.Network, NodeEntryPoint.Farmer);
+            var certFilePath = Path.Combine(certsPath, "private_farmer.crt");
+            var keyFilePath = Path.Combine(certsPath, "private_farmer.key");
+
+            var nodeApiConfig = new ChiaApiConfig(certFilePath, keyFilePath, "localhost", chainConfig.FullNodePort);
+            return new FarmerApiClient(nodeApiConfig);
+        }
+
+        internal static HarvesterApiClient GetHarvesterApiClient(IChainConfig chainConfig)
+        {
+            var certsPath = FileSystem.GetChainCertDirectoryPath(chainConfig.ChainFolder, chainConfig.Network, NodeEntryPoint.Harvester);
+            var certFilePath = Path.Combine(certsPath, "private_harvester.crt");
+            var keyFilePath = Path.Combine(certsPath, "private_harvester.key");
+
+            var nodeApiConfig = new ChiaApiConfig(certFilePath, keyFilePath, "localhost", chainConfig.FullNodePort);
+            return new HarvesterApiClient(nodeApiConfig);
+        }
+
+        internal static WalletApiClient GetWalletApiClient(IChainConfig chainConfig)
+        {
+            var certsPath = FileSystem.GetChainCertDirectoryPath(chainConfig.ChainFolder, chainConfig.Network, NodeEntryPoint.Wallet);
+            var certFilePath = Path.Combine(certsPath, "private_wallet.crt");
+            var keyFilePath = Path.Combine(certsPath, "private_wallet.key");
+
+            var nodeApiConfig = new ChiaApiConfig(certFilePath, keyFilePath, "localhost", chainConfig.FullNodePort);
+            return new WalletApiClient(nodeApiConfig);
+        }
+    }
+}
